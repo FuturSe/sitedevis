3fc7ebc403943e42341eaa44485615c99d0455ff
FuturSe <sokelaurent@gmail.com>
2024-02-18 21:28:27 -0300

    init

--- FormulaireDevis.php
+++ FormulaireDevis.php
@@ -42,7 +42,7 @@
         ?>
     </head>
     <body>
-        <h2>maman</h2>
+      
         <h2>Formulaire Devis </h2>
         <form action="TraitementFormulaire.php" method="post">
             <label for="nom_entreprise">Nom de l'entreprise : </label>
